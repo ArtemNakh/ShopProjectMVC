d8518a7f75244d9aabb5971450d43602ec3e77f6
Artem Nakhman <artem.nakhman@gmail.com>
2024-07-27 17:01:36 +0300

    Add project files.

---
+++ ShopProjectMVC-main/ShopProjectMVC/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopProjectMVC.Core.Interfaces;
+
+namespace ShopProjectMVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IActionResult Index()
+        {
+            var orders = _productService.GetAll();
+            return View(orders);
+        }
+    }
+}
